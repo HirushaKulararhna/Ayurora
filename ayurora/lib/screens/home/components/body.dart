6ffc395f540b5d59386b0d879c81ebcf1308e12d
Hirusha Kularathna <kularathnahirusha@gmail.com>
2025-09-21 18:11:22 +0530

    completed header and search box
    project setup completed
    create header
    create seachbar

---
+++ ayurora/lib/screens/home/components/body.dart
@@ -0,0 +1,24 @@
+import 'package:ayurora/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'header_with_searchbox.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // it will provide us total height and width of our screen
+    Size size = MediaQuery.of(context).size;
+    // it enable scrolling on small devices
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          HeaderWithSearchBox(size: size)
+        ],
+      
+      ),
+    );
+  }
+}
+
---
+++ ayurora/lib/screens/home/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:ayurora/constants.dart';
+import 'package:ayurora/screens/home/components/body.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: buildAppBar(),
+        body: Body(),
+      ); // Replace with actual home screen implementation
+    
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+        elevation: 0,
+        backgroundColor: kPrimaryColor,
+        leading: IconButton(
+          icon: SvgPicture.asset("assets/icons/menu.svg"),
+          onPressed: () {},
+        ),
+      );
+  }
+}
