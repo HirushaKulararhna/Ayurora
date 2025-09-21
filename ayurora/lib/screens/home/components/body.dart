99175431dbc3220570c72742814230e212985df4
Hirusha Kularathna <kularathnahirusha@gmail.com>
2025-09-22 00:41:51 +0530

    create recomend_pant.dart file

--- ayurora/lib/screens/home/components/body.dart
+++ ayurora/lib/screens/home/components/body.dart
@@ -2,6 +2,7 @@ import 'package:ayurora/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'header_with_searchbox.dart';
+import 'recomend_plants.dart';
 import 'title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
@@ -16,7 +17,9 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
-          TitleWithMoreBtn(title: "Recommended", press: () {},),
+          TitleWithMoreBtn(title: "Recommended", press: () {}),
+          // It will cover 40% of our total witdh
+          RecomendsPlants(),
         ],
       ),
     );
